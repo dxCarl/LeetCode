0a0d184605d4447937762113df36a2205d6b8424
745125420 <745125420@qq.com>
2020-03-08 13:51:44 +0800

    findContinuousSequence

---
+++ src/main/java/com/xiao/deng/leetcode/Sequence/Solution.java
@@ -0,0 +1,60 @@
+package com.xiao.deng.leetcode.Sequence;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+ * 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+ * <p>
+ * 输入：target = 9
+ * 输出：[[2,3,4],[4,5]]
+ * <p>
+ * 输入：target = 15
+ * 输出：[[1,2,3,4,5],[4,5,6],[7,8]]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    public int[][] findContinuousSequence(int target) {
+
+        int i = 1; // 滑动窗口的左边界
+        int j = 1; // 滑动窗口的右边界
+        int sum = 0; // 滑动窗口中数字的和
+        List<int[]> res = new ArrayList<>();
+
+        while (i <= target / 2) {
+            if (sum < target) {
+                // 右边界向右移动
+                sum += j;
+                j++;
+            } else if (sum > target) {
+                // 左边界向右移动
+                sum -= i;
+                i++;
+            } else {
+                // 记录结果
+                int[] arr = new int[j - i];
+                for (int k = i; k < j; k++) {
+                    arr[k - i] = k;
+                }
+                res.add(arr);
+                // 左边界向右移动
+                sum -= i;
+                i++;
+            }
+        }
+
+        return res.toArray(new int[res.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int target = 15;
+        int[][] ans = new Solution().findContinuousSequence(target);
+        System.out.println(ans.length);
+    }
+}
