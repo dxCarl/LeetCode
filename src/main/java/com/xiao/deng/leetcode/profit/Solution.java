900a8ca76e775e5d3b41ec37b106f587628d76cf
745125420 <745125420@qq.com>
2020-03-09 22:50:47 +0800

    max profit

---
+++ src/main/java/com/xiao/deng/leetcode/profit/Solution.java
@@ -0,0 +1,43 @@
+package com.xiao.deng.leetcode.profit;
+
+/**
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ * 注意你不能在买入股票前卖出股票。
+ * <p>
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len == 0) {
+            return 0;
+        }
+        int min = prices[0];
+        int profit = 0;
+        for (int i = 0; i < len; i++) {
+            int cur = prices[i];
+            if (cur < min) {
+                min = cur;
+            }
+            int curPro = cur - min;
+            profit = profit > curPro ? profit : curPro;
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        int ans = new Solution().maxProfit(prices);
+        System.out.println(ans);
+
+    }
+}
